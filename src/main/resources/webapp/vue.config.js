f09b3f1b0c58f7188310cfa7f5acc5b1723c6333
wengyijiong <wengyijiong@zfzx.cn>
2019-01-02 14:09:26 +0800

    * vue 代码

---
+++ src/main/resources/webapp/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+      proxy: {
+          '/api': {
+              target: 'http://127.0.0.1:8088/',
+              changeOrigin: true,
+              ws: true,
+              pathRewrite: {
+                '^/api': ''
+              }
+          }
+      }
+  }
+}
